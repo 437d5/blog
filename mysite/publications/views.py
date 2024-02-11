0563fa60ff44ec8248e719b2b91937432caefebf
437d5 <uladzislaukaplan@gmail.com>
2024-02-11 20:37:08 +0300

    Hola amigo buenos dias

--- mysite/publications/views.py
+++ mysite/publications/views.py
@@ -10,6 +10,7 @@ def index(request):
     context = {
         "publications": publications
     }
+
     return render(request, template_name=template_name, context=context)
 
 def detail(request, pk):
